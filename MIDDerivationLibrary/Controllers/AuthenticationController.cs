4e10a4aec9452dae4c2f8d991639af39ececdb50
Sandip Patil <sandip.patil@nitorinfotech.com>
2021-04-26 15:09:29 +0530

    Token related code changes

--- MIDDerivationLibrary/Controllers/AuthenticationController.cs
+++ MIDDerivationLibrary/Controllers/AuthenticationController.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MIDDerivationLibrary.Controllers
@@ -11,5 +16,33 @@ namespace MIDDerivationLibrary.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
+        private IConfiguration _config;
+        public AuthenticationController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [Route("GenerateToken")]
+        [HttpGet]
+        public IActionResult GenerateToken()
+        {
+            IActionResult response = Unauthorized();
+            var tokenString = GenerateJSONWebToken();
+            response = Ok(new { token = tokenString });
+            return response;
+        }
+
+        private string GenerateJSONWebToken()
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Issuer"],null,expires: null,
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
     }
 }
